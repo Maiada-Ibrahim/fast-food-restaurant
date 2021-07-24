934d2d3e8723ac8bb53cf10c33c4b90316333c69
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-07-24 21:47:05 +0300

    j

--- js/app.js
+++ js/app.js
@@ -1,59 +1,69 @@
 "use strict";
 
 let formEL = document.getElementById('myform')
-let selectEL = document.getElementById('myselect')
-formEL.addEventListener('submit', fun)
+//let selectEL = document.getElementById('myselect')
+formEL.addEventListener('submit', funsubmit)
 let tableEL = document.getElementById('mytable')
 let tbodyEL = document.getElementById('tbody')
-let mainEL = document.getElementById('main')
 tableEL.appendChild(tbodyEL)
 let orderarray = []
 
 let username
 let foodtype
-let order1
-function fun(event) {
+let foodimage
+let price
+let chesese
+let cheesecheckbox = document.getElementById('cheese')
+let pepsicheckboxE = document.getElementById('pepsi')
+
+let pepsiorpotato
+function funsubmit(event) {
 
     event.preventDefault();
     username = event.target.username.value
     foodtype = event.target.myselect.value
-    console.log(username)
-    console.log(foodtype)
-  
-  
-    let foodimage = new Image(100, 200);
+    // let checkcheeseEL=event.target.cheese.value
+
 
     if (foodtype == 'shawrma') {
-        foodimage.src = 'img/shawerma.png';
+        foodimage = 'img/shawerma.png';
 
-        let price = randomprice(1.20, 3.50)
-        console.log(price)
-        let order1 = new Order(username, foodtype, foodimage, price);
+        if (cheesecheckbox.checked == true) { chesese = 'with extra cheese' } else { chesese = 'no extra chesese' }
+        if (pepsicheckboxE.checked == true) { pepsiorpotato = 'with pepsi' } else { pepsiorpotato = 'with potato' }
+        price = randomprice(1.20, 3.50) + 'JD'
+        let order1 = new Order(username, foodtype, foodimage, chesese, pepsiorpotato, price);
 
 
 
     } else if (foodtype == 'burger') {
-        foodimage.src = 'img/burger.jpg';
-        let price = randomprice(2.50, 6.50)
-        let order1 = new Order(username, foodtype, foodimage, price);
+        foodimage = 'img/burger.jpg';
+
+        if (cheesecheckbox.checked == true) { chesese = 'with extra cheese' } else { chesese = 'no extra chesese' }
+
+
+        price = randomprice(2.50, 6.50) + 'JD'
+        let order1 = new Order(username, foodtype, foodimage, chesese, price);
 
 
     } else {
-        foodimage.src = 'img/pizza.png';
+        foodimage = 'img/pizza.png';
+        if (cheesecheckbox.checked == true) { chesese = 'with extra cheese' } else { chesese = 'no extra chesese' }
+        price = randomprice(3.5, 11) + 'JD'
+        let order1 = new Order(username, foodtype, foodimage, chesese, price);
 
-        let price = randomprice(3.5, 11)
-
-        order1 = new Order(username, foodtype, foodimage, price);
-        
 
     }
-      let rowscount = tableEL.rows.length
-    for(let i=1 ;i<rowscount;i++){
-        tableEL.deleteRow(1);     //لانه في صف للعنوان
+
+    // let rowscount = tableEL.rows.length
+    // for (let i = 1; i < rowscount; i++) {
+    //     tableEL.deleteRow(1);    
+    // }
+    let rowCount = tableEL.rows.length;
+    for (let i = rowCount - 1; i > 0; i--) {
+        tableEL.deleteRow(i);
     }
-    
     readlocal()
-    
+
 }
 
 
@@ -66,11 +76,13 @@ function randomprice(min, max) {
 
 
 
-function Order(username, foodtype, foodimage, price) {
+function Order(username, foodtype, foodimage, chesese, pepsiorpotato, price) {
     this.username = username;
     this.foodtype = foodtype;
     this.foodimage = foodimage;
+    this.chesese = chesese;
     this.price = price;
+    this.pepsiorpotato = pepsiorpotato;
     orderarray.push(this)
     localStorage.setItem("orderobject", JSON.stringify(orderarray));
 }
@@ -88,16 +100,30 @@ function rendertbody() {
         let trEL = document.createElement('tr')
 
         let tddetalsEL = document.createElement('td')
-        tddetalsEL.textContent = `${orderarray[i].username}  ${orderarray[i].foodtype}` + `${orderarray[i].price}`
-
-        // let tdfoodimageEL = document.createElement('td')
-        // let divel = document.createElement('div')
-        // divel.appendChild(orderarray[i].foodimage)
-        // tdfoodimageEL.appendChild(divel)
+        tddetalsEL.setAttribute('style', 'white-space: pre;');
+        tddetalsEL.textContent = `${orderarray[i].username} \r\n ${orderarray[i].foodtype}\r\n` + `${orderarray[i].price}\r\n` + `${orderarray[i].chesese}\r\n` + `${orderarray[i].pepsiorpotato}`
 
 
-    //    trEL.appendChild(tdfoodimageEL)
+        let foodimageEL = new Image(100, 200);
+        foodimageEL.src = orderarray[i].foodimage
+        let tdfoodimageEL = document.createElement('td')
+        let divel = document.createElement('div')
+        divel.appendChild(foodimageEL)
+        tdfoodimageEL.appendChild(divel)
+
+        let removeEL = document.createElement('td')
+        let removebtnEL = document.createElement('button')
+        removebtnEL.textContent = 'remove'
+        removebtnEL.style.width = "60px"
+        removebtnEL.style.height = '20px'
+        removebtnEL.id = `${i}`
+        removeEL.appendChild(removebtnEL)
+        removebtnEL.addEventListener('click', removefun)
+
+
+        trEL.appendChild(tdfoodimageEL)
         trEL.appendChild(tddetalsEL)
+        trEL.appendChild(removeEL)
         tbodyEL.appendChild(trEL)
 
 
@@ -117,20 +143,36 @@ function readlocal() {
 
         orderarray = ourArray
         console.log(orderarray)
-
         //  let order1 = new Order(orderarray[i].username, orderarray[i].foodtype, orderarray[i].foodimage, orderarray[i].price);
         rendertbody()
 
 
 
     } else {
-        
-     }
+
+    }
 
 }
 
 
+function removefun(event) {
+    event.preventDefault();
+    let rowid = event.target.id
+    console.log(rowid)
+    for (let i = 0; i < orderarray.length; i++) {
+        if (i == rowid) {
+            orderarray.splice(i, 1)
+        }
 
+    }
+    localStorage.setItem("orderobject", JSON.stringify(orderarray));
+    let rowCount = tableEL.rows.length;
+    for (let i = rowCount - 1; i > 0; i--) {
+        tableEL.deleteRow(i);
+    }
+    readlocal()
+
+}
 
 
 
